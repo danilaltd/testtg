62f81ac95726d3792dd87d5b2aa5985d5bedb365
danilaltd <danilaltd2006@gmail.com>
2025-05-07 17:47:55 +0300

    add port listen once again

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3dac49d20f3033cbb88d168c4763a8589dd7522")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5f42781e1a5023a7ae93da676b8f7513fd4f51a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
